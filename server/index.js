df76583025d4ca60d36eaea1195e954a108e141b
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-10-13 23:27:43 +0530

    add chat route & chat controller

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
 dotenv.config();
 connectDB();
@@ -9,7 +11,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use("/api/user", userRoutes);
-app.use("/api/chat", chatRoutes);
+// app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 const PORT = process.env.PORT;
